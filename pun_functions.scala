8f5d1477424e5138f00f6b7d6cf060d3f7e2cbc2
davideflo <davideflo@users.noreply.github.com>
2016-08-09 17:21:27 +0200

    first commit with scala code
    started translation into scala of the code used to create the datasets to build DL models in R. Reason: speed.

---
+++ pun_functions.scala
@@ -0,0 +1,40 @@
+import scala.math
+
+def ConvertDay(day: String): String = {
+	val converter = Map("Sun" -> "dom",
+		"Mon" -> "lun",
+		"Tues" -> "mar",
+		"Wed" -> "mer",
+		"Thurs" -> "gio",
+		"Fri" -> "ven",
+		"Sat" -> "sab")
+
+	converter(day)
+}
+/*********************************************************/
+def convert_day_to_angle(day: String): Double = {
+	val converter = Map("dom" -> 1,
+		"lun" -> 2,
+		"mar" -> 3,
+		"mer" -> 4,
+		"gio" -> 5,
+		"ven" -> 6,
+		"sab" -> 7)
+	scala.math.cos(converter(day) * scala.math.Pi * (1/7))
+}
+/*********************************************************/
+def convert_hour_to_angle(ora: Array[Int]): Double = {
+	ora.map(x => scala.math.sin((x - 12) * scala.math.Pi * (1/24)))
+}
+/*********************************************************/
+def subsequent_day(day: String) : String = {
+	val converter = Map("dom" -> "lun",
+		"lun" -> "mar",
+		"mar" -> "mer",
+		"mer" -> "gio",
+		"gio" -> "ven",
+		"ven" -> "sab",
+		"sab" -> "dom")
+	converter(day)
+}
+/**********************************************************/
